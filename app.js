480e27306a6d326b501b0dd0f360e92dedf8a276
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-22 14:28:34 +0200

    ADDED: Lampadina si accende

---
+++ app.js
@@ -0,0 +1,7 @@
+const lampadina = document.getElementById('lampadina')
+const bottone = document.getElementById('lampadinabtn')
+
+bottone.addEventListener('click', function() {
+    lampadina.src = './img/img/yellow_lamp.png'
+}
+);

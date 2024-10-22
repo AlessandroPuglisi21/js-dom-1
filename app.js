6dfd41ca15a27af59c9ea246312b6cb29e97d0a0
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-22 16:33:02 +0200

    ADDED: On/Off

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const lampadina = document.getElementById('lampadina')
-const bottone = document.getElementById('lampadinabtn')
+const lampadina = document.getElementById("lampadina");
+const bottone = document.getElementById("lampadinabtn");
 
-bottone.addEventListener('click', function() {
-    lampadina.src = './img/img/yellow_lamp.png'
-}
-);
+// bottone.addEventListener('click', function() {
+//     lampadina.src = './img/img/yellow_lamp.png'
+// }
+// );
+// bottone.addEventListener('click', function() {
+//     this.textContent = 'Spegni'
+// }
+// );
+
+bottone.addEventListener("click", function () {
+    console.log(lampadina.src)
+ if (lampadina.src.includes("white_lamp.png")) {
+    lampadina.src = "./img/img/yellow_lamp.png";
+    bottone.textContent = "Accendi";
+  } else {
+    lampadina.src = "./img/img/white_lamp.png";
+    bottone.textContent = "Spegni";
+  }
+});
